056c13a1374d34c6fb110b2561f0c2d66bb41206
Pooja22112000 <73222879+Pooja22112000@users.noreply.github.com>
2021-06-18 17:12:25 +0530

    Add files via upload

---
+++ SwapWithout.java
@@ -0,0 +1,13 @@
+//swapping without temporary variable
+import java.lang.*;
+public class SwapWithout{
+	public static void main(String[] args){
+		int a=3, b=9;
+		System.out.println(" A.POOJA 18MIS0232");
+		System.out.println("The values before Swapping: \n a="+a+"b="+b);
+		a=a-b;
+		b=a+b;
+		a=b-a;
+		System.out.println("The values after Swapping: \n a="+a+"b="+b);
+	}
+}
